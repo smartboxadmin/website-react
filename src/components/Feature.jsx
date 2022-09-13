736bf9f075e85ef0929da6535e01b87a2609adf5
Indrajeet Aditya Roy <indrajeetadityaroy@Indrajeets-MacBook-Pro.local>
2022-09-12 23:05:19 -0700

    Edited platform feature info

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -10,7 +10,7 @@ export default function Feature() {
             Do not allow lack of time, research or resources hold you back from good investment decisions and choices.
           </h3>
           <p className="description">
-          Many regular retail investors only follow the news or reddit forums when making investment decisions and are often mislead into wasting their efforts and money. Smartbox's are the perfect solution, as they are a collection of investment suggestions formulated on the basis of research and experience of experts in the fields of trading and finance.
+          People often follow the news or reddit forums when making investment decisions and are mislead into wasting their efforts and money. Smartbox's are the perfect solution, as they are a collection of investment suggestions formulated on the basis of research and experience of experts in the fields of trading and finance.
           </p>
         </div>
         <div className="content">
@@ -19,16 +19,16 @@ export default function Feature() {
             What is a smartbox?
           </h2>
           <p className="description">
-          A smartbox is basket or collection of stocks/ETFs formulated around on a particular theme such as Green Energy, Housing Sector Demand, Top Dividends, Pharma makers etc. Smartbox's have been put together by financial analysts/advisors on the basis of their knowledge and experience.
+          A smartbox is basket or collection of stocks/ETFs formulated around particular themes such as Green Energy, Housing Sector Demand, Top Dividends etc. Smartbox's have been put together by financial analysts/advisors on the basis of their knowledge and experience.
           </p>
         </div>
         <div className="content">
           <h1 className="title">Investors & Experts</h1>
           <h2 className="description">
-            Smartbox is not only built for regular retail investors but also for financial advisors and analysts.
+            Smartbox is not only built for retail investors but also for financial advisors and analysts.
           </h2>
           <p className="description">
-            Experts in the fields of finance and trading can share their knowledge and advice in the form of smartboxes and grow their client list and profit margins.
+            Experts in the fields of finance and trading can share their knowledge and advice in the form of smartboxes, at the same time growing their client lists & increasing their profits.
           </p>
         </div>
       </div>
